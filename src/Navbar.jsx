a5803505b197d1337052365605668a9a8a16df64
crossroads30 <crossroads30@mail.ru>
2024-01-03 16:22:53 +0300

    fix: fix item name

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -26,8 +26,8 @@ function Navbar() {
 					</ul>
 				</div>
 				<ul className='social-icons'>
-					{social.map(icon => {
-						const { id, url, icon } = icon
+					{social.map(socialIcon => {
+						const { id, url, icon } = socialIcon
 						return (
 							<li key={id}>
 								<a href={url}>{icon}</a>
